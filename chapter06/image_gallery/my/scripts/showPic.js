315ca01181f2e9fa5de19a665bd24afe1754bac8
王帥 <believewangshuai@126.com>
2016-11-30 00:36:31 +0800

    bug

---
+++ chapter06/image_gallery/my/scripts/showPic.js
@@ -0,0 +1,36 @@
+function showPic(whichpic) {
+	if (!document.getElementById("placeholder")) return false;
+	var source=whichpic.getAttribute("href");
+	var placeholer=document.getElementById("placeholer");
+	placeholer.setAttribute("src",source);
+	if (document.getElementById("description")){
+		var text=whichpic.getAttribute("title");
+		var description=document.getElementById("description");
+		description.firstChild.nodeValue=text;
+	}
+	return true;
+}
+function prepareGallery() {
+	if (!document.getElementsByTagName) return false;
+	if (!document.getElementById) return false;
+	if (!document.getElementById("imagegallery")) return false;
+	var gallery=document.getElementById("imagegallery");
+	var links=gallery.getElementsByTagName('a');
+	for (var i = 0; i < links.length; i++) {
+		links[i].onclick=function () {
+			return !showPic(this);
+		}
+	}
+}
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function() {
+			oldonload();
+			func();
+		}
+	}
+}
+addLoadEvent(prepareGallery);
